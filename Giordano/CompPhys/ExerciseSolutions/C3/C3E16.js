10212bfa1ee0d4e4ffa25eff8ec09cbec699a352
David L. Goldsmith <OlyDLG@users.noreply.github.com>
2025-08-25 22:30:20 -0700

    Added phase plane plot; now I need to add mod 2pi

--- Giordano/CompPhys/ExerciseSolutions/C3/C3E16.js
+++ Giordano/CompPhys/ExerciseSolutions/C3/C3E16.js
@@ -21,23 +21,39 @@ function GiorCPC3E16() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 16
         let t = 0;
         let fn = [0.2, 0];
         let fnp1 = [0, 0];
-        const data = [['t', '\u03B8(t)', '\u03C9(t)']];
+        const tdData = [['t', '\u03B8(t)', '\u03C9(t)']];
+        const ppData = [['\u03B8', '\u03C9']];
         for (let i=0; i < N; i++) {
-            data.push([t, fn[0], fn[1]]);
+            tdData.push([t, fn[0], fn[1]]);
+            ppData.push([fn[0], fn[1]]);
             fnp1 = RK42Dnonauton(dxdt, fn, t, h); // 4th order RK for non-autonomous 2D system
             t += h;
             fn = fnp1;
         }
-        const dtable = google.visualization.arrayToDataTable(data);
-        const title = "<center><h3>Solution of Physical Pendulum Model</h3></center>";
-        const options = {...globalChartOptions,
-                         hAxis: {title: 'Time'},
-                         vAxis: {title: '\u03B8: radians, \u03C9: rad/sec'},
-                         width: 1000, height: 400};
-        // Display the chart inside the <div> element with id="GiorCPC3E16chart"
-        const chart = new google.visualization.LineChart(document.getElementById("GiorCPC3E16chart"));
-        chart.draw(dtable, options);
-        dsEBIiH("GiorCPC3E16chartTitle", title);
+
+     // Time domain chart creation and placement
+        const tdTable = google.visualization.arrayToDataTable(tdData);
+        const tdOptions = {...globalChartOptions,
+                           hAxis: {title: 'Time'},
+                           vAxis: {title: '\u03B8: radians, \u03C9: rad/sec'},
+                           width: 500, height: 400};
+        // Display the chart inside the <div> element with id="GiorCPC3E16tdChart"
+        const tdChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E16tdChart"));
+        tdChart.draw(tdTable, tdOptions);
+
+     // Phase plane chart creation and placement
+        const ppTable = google.visualization.arrayToDataTable(ppData);
+        const ppOptions = {...globalChartOptions,
+                           hAxis: {title: '\u03B8 (radians)'},
+                           vAxis: {title: '\u03C9 (rad/sec)'},
+                           pointSize: 1, lineWidth: 0, legend: 'none', 
+                           width: 500, height: 400};
+        // Display the chart inside the <div> element with id="GiorCPC3E16ppChart"
+        const ppChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16ppChart"));
+        ppChart.draw(ppTable, ppOptions);
+
+        const title = "<center><h3>Solutions of Physical Pendulum Model</h3></center>";
+        dsEBIiH("GiorCPC3E16Title", title);
     }
     makeC3E16Graph();
     return makeC3E16Graph;
