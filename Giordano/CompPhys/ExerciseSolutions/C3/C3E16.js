0317a5f721d12822b1be76be55df0e7a49951baa
David L. Goldsmith <OlyDLG@users.noreply.github.com>
2025-08-26 23:27:51 -0700

    Everything's basically working...
    but I still need to figure out a best way of filling in the attractor

--- Giordano/CompPhys/ExerciseSolutions/C3/C3E16.js
+++ Giordano/CompPhys/ExerciseSolutions/C3/C3E16.js
@@ -9,7 +9,7 @@ function GiorCPC3E16() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 16
     const elForm = document.getElementById("C3E16el");
     const hForm = document.getElementById("C3E16h");
     const tmForm = document.getElementById("C3E16tmin");
-    const tMForm = document.getElementById("C3E16tmax");
+    const NForm = document.getElementById("C3E16N");
 
     function makeC3E16Graph() {
         function dxdt(x, t) { // For use with RK42Dnonauton
@@ -17,10 +17,22 @@ function GiorCPC3E16() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 16
             let temp = -1*gol*Math.sin(x1) - q*[x2] + FD*Math.sin(WD*t);
             return [x2, temp];
         }
+        const N = Number(NForm[0].value);
+        const swtch = (N < 7500);
+        if (swtch) {
+          document.getElementById("E16tsppGraphTable").hidden = false;
+          document.getElementById("E16saTitleID").hidden = true;
+          document.getElementById("GiorCPC3E16saChart").hidden = true;
+        }
+        else {
+          document.getElementById("E16tsppGraphTable").hidden = true;
+          document.getElementById("E16saTitleID").hidden = false;
+          document.getElementById("GiorCPC3E16saChart").hidden = false;
+        }
         const h = Number(hForm[0].value);
         const h2 = h / 2;
         const tmin = Number(tmForm[0].value);
-        const tmax = tmin + Number(tMForm[0].value);
+        const tmax = tmin + N * h;
         const FD = Number(FDForm[0].value);
         const WD = Number(WDForm[0].value) / 48;
         const WDo2pi = WD / tpi;
@@ -34,69 +46,69 @@ function GiorCPC3E16() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 16
         const saData = [['\u03B8', '\u03C9']];
         let lb = 0, ub = 0;
         while (t < tmax) {
-            if (t >= tmin) {
-                tdData.push([t, fn[0], fn[1]]);
-                ppData.push([fn[0], fn[1]]);
-                lb = WDo2pi * (t-h2); ub = WDo2pi * (t+h2);
-                if (Math.ceil(lb) == Math.floor(ub)) {
-                    saData.push([fn[0], fn[1]]);
-                }
+          if (t >= tmin) {
+            if (swtch) {
+              tdData.push([t, fn[0], fn[1]]);
+              ppData.push([fn[0], fn[1]]);
             }
-            fnp1 = RK42Dnonauton(dxdt, fn, t, h); // 4th order RK for non-autonomous 2D system
-            if (fnp1[0] > pi) {
-               fnp1[0] -= tpi;
+            else {
+              lb = WDo2pi * (t-h2); ub = WDo2pi * (t+h2);
+              if (Math.ceil(lb) == Math.floor(ub)) {
+                saData.push([fn[0], fn[1]]);
+              }
             }
-            else if (fnp1[0] < npi) {
-               fnp1[0] += tpi;
-            }
-            t += h;
-            fn = fnp1;
+          }
+          fnp1 = RK42Dnonauton(dxdt, fn, t, h); // 4th order RK for non-autonomous 2D system
+          if (fnp1[0] > pi) {
+            fnp1[0] -= tpi;
+          }
+          else if (fnp1[0] < npi) {
+            fnp1[0] += tpi;
+          }
+          t += h;
+          fn = fnp1;
         }
+        if (swtch) { // Time domain chart creation and placement
+          const tdTable = google.visualization.arrayToDataTable(tdData);
+          const tdOptions = {...globalChartOptions,
+                             hAxis: {title: 'Time'},
+                             vAxis: {title: '\u03B8: radians, \u03C9: rad/sec',
+                                     viewWindowMode: 'explicit',
+                                     viewWindow: {min: -3.5, max: 3.5}},
+                             width: 500, height: 400};
+          const tdChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E16tdChart"));
+          tdChart.draw(tdTable, tdOptions);
 
-     // Time domain chart creation and placement
-        const tdTable = google.visualization.arrayToDataTable(tdData);
-        const tdOptions = {...globalChartOptions,
-                           hAxis: {title: 'Time'},
-                           vAxis: {title: '\u03B8: radians, \u03C9: rad/sec',
-                                   viewWindowMode: 'explicit',
-                                   viewWindow: {min: -3.5, max: 3.5}},
-                           width: 500, height: 400};
-        // Display the chart inside the <div> element with id="GiorCPC3E16tdChart"
-        const tdChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E16tdChart"));
-        tdChart.draw(tdTable, tdOptions);
-
-     // Phase plane chart creation and placement
-        const ppTable = google.visualization.arrayToDataTable(ppData);
-        const ppOptions = {...globalChartOptions,
-                           hAxis: {title: '\u03B8 (radians)',
-                                   ticks: [{v: npi, f: '-\u03C0'},
-                                           {v: 0, f: '0'},
-                                           {v: pi, f: '\u03C0'}]},
-                           vAxis: {title: '\u03C9 (rad/sec)',
-                                   viewWindowMode: 'explicit',
-                                   viewWindow: {min: -2.5, max: 2.5}},
-                           pointSize: 1, lineWidth: 0, legend: 'none', 
-                           width: 500, height: 400};
-        // Display the chart inside the <div> element with id="GiorCPC3E16ppChart"
-        const ppChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16ppChart"));
-        ppChart.draw(ppTable, ppOptions);
-
-     // Strange attractor chart creation and placement
-        const saTable = google.visualization.arrayToDataTable(saData);
-        const saOptions = {...globalChartOptions,
-                           hAxis: {title: '\u03B8 (radians)',
-                                   ticks: [{v: npi, f: '-\u03C0'},
-                                           {v: 0, f: '0'},
-                                           {v: pi, f: '\u03C0'}]},
-                           vAxis: {title: '\u03C9 (rad/sec)',
-                                   viewWindowMode: 'explicit',
-                                   viewWindow: {min: -2, max: 1}},
-                           pointSize: 1, lineWidth: 0, legend: 'none', 
-                           width: 500, height: 400};
-        // Display the chart inside the <div> element with id="GiorCPC3E16saChart"
-        const saChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16saChart"));
-        saChart.draw(saTable, saOptions);
-
+        // Phase plane chart creation and placement
+          const ppTable = google.visualization.arrayToDataTable(ppData);
+          const ppOptions = {...globalChartOptions,
+                             hAxis: {title: '\u03B8 (radians)',
+                                     ticks: [{v: npi, f: '-\u03C0'},
+                                             {v: 0, f: '0'},
+                                             {v: pi, f: '\u03C0'}]},
+                             vAxis: {title: '\u03C9 (rad/sec)',
+                                     viewWindowMode: 'explicit',
+                                     viewWindow: {min: -2.5, max: 2.5}},
+                             pointSize: 1, lineWidth: 0, legend: 'none', 
+                             width: 500, height: 400};
+          const ppChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16ppChart"));
+          ppChart.draw(ppTable, ppOptions);
+        }
+        else { // Strange attractor chart creation and placement
+          const saTable = google.visualization.arrayToDataTable(saData);
+          const saOptions = {...globalChartOptions,
+                             hAxis: {title: '\u03B8 (radians)',
+                                     ticks: [{v: npi, f: '-\u03C0'},
+                                             {v: 0, f: '0'},
+                                             {v: pi, f: '\u03C0'}]},
+                             vAxis: {title: '\u03C9 (rad/sec)',
+                                     viewWindowMode: 'explicit',
+                                     viewWindow: {min: -2, max: 1}},
+                             pointSize: 1, lineWidth: 0, legend: 'none', 
+                             width: 500, height: 400};
+          const saChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16saChart"));
+          saChart.draw(saTable, saOptions);
+        }
         const title = "<center><h3>Solutions of Physical Pendulum Model</h3></center>";
         dsEBIiH("GiorCPC3E16Title", title);
     }
