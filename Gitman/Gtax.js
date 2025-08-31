a9cb02c01d837fee0c8b5c95a4076b2d251e0c53
David L. Goldsmith <OlyDLG@users.noreply.github.com>
2025-08-31 16:14:50 -0700

    Added Source code links and Contents sections,
    added a title, replaced <style> w/ css link,  replaced Utilities call w/ GeneralUtils call, implemented goback protocol, added link to my LI page; and replaced vars w/ lets in src code

--- Gitman/Gtax.js
+++ Gitman/Gtax.js
@@ -1,23 +1,22 @@
 function GitmanTax(income) {
-    var tax = (income < 50000) ? 0.15 * income : ( 
-                     (income < 75000) ? 7500 + 0.25 * (income - 50000) : (
-                      (income < 100000) ? 13750 + 0.34 * (income - 75000) : (
-                       (income < 335000) ? 22250 + 0.39 * (income - 100000)  : (
+  let tax = (income < 50000) ? 0.15 * income : ( 
+               (income < 75000) ? 7500 + 0.25 * (income - 50000) : (
+                  (income < 100000) ? 13750 + 0.34 * (income - 75000) : (
+                     (income < 335000) ? 22250 + 0.39 * (income - 100000)  : (
                         (income < 10000000) ? 113900 + 0.34 * (income - 335000) : (
-                             3400000 + 0.35 * (income - 10000000))))));
-    return tax;
+                           3400000 + 0.35 * (income - 10000000))))));
+  return tax;
 }
 
 function GitmanAvTax(income) {
-    return GitmanTax(income)/income;
+  return GitmanTax(income)/income;
 }
 
 function GitmanMargTax(income) {
-    var margtax = (income < 49999) ? 0.15 : ( 
-                               (income < 74999) ? 0.25 : (
-                                (income < 99999) ? 0.34 : (
-                                 (income < 334999) ? 0.39 : (
-                                  (income < 9999999) ? 0.34 : (
-                                                       		          0.35)))));
-    return margtax;
+  let margtax = (income < 49999) ? 0.15 : ( 
+                   (income < 74999) ? 0.25 : (
+                      (income < 99999) ? 0.34 : (
+                         (income < 334999) ? 0.39 : (
+                            (income < 9999999) ? 0.34 : (0.35)))));
+  return margtax;
 }
