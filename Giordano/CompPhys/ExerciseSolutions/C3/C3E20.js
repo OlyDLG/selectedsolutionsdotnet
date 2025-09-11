c552a6e94db3c81087fd55caceea46a1fbbd7d90
David L. Goldsmith <OlyDLG@users.noreply.github.com>
2025-09-10 22:48:05 -0700

    Basic bifurcation diagrams working...
    but need to work on the zoom-in, and figure out how to either speed things up or show the plot while its being generated.

--- Giordano/CompPhys/ExerciseSolutions/C3/C3E20.js
+++ Giordano/CompPhys/ExerciseSolutions/C3/C3E20.js
@@ -1,20 +1,48 @@
 function GiorCPC3E20() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 20
-  const g = 9.8;
-  const pi = Math.PI;
-  const npi = -1 * pi;
-  const tpi = 2 * pi;
-  const WD = 2.0 / 3;
-  const T = tpi / WD;
-  const tmin = 0, tmax = 400 * T, Xient = 300 * T;
-  const h = 0.001 * T, h2 = h / 2;
-  const FDmin = 1.42;
-  const FDmax = 1.49;
-  const dFDmain = 0.001;
-  const dFDdetail = 0.0001;
-//  let FD2 = 1.424;
-//  const FD4 = 1.459;
-//  const FD8 = 1.476;
-//  const FD16 = 1.4775;
+  const g = 9.8, 
+        pi = Math.PI, 
+        npi = -1 * pi,
+        tpi = 2 * pi,
+        WD = 2.0 / 3, 
+        T = tpi / WD,
+        tmin = 0, 
+        tmax = 400 * T, 
+        Xient = 300 * T,
+        FDmin = 1.42, 
+        FDmax = 1.49,
+        dFDmain = 0.001, 
+        dFDdetail = 0.0001,
+        swtchForm = document.getElementById("C3E20swtch"),
+        mainData = [['FD', 'theta']],
+//        mainTable = google.visualization.arrayToDataTable(mainData),
+        mainOptions = {...globalChartOptions,
+                       hAxis: {title: 'F_D',
+                               viewWindowMode: 'explicit',
+                               viewWindow: {min: 1.42, max: 1.49}},
+                       vAxis: {title: '\u03B8 (radians)',
+                               viewWindowMode: 'explicit',
+                               viewWindow: {min: 1, max: 3}},
+                       pointSize: 1, legend: 'none', //lineWidth: 0,  
+                       width: 500, height: 400},
+        mainChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E20bifdiaChart")),
+//      mainChart.draw(mainTable, mainOptions);
+        detData = [['FD', 'theta']],
+//        detTable = google.visualization.arrayToDataTable(detData),
+        detOptions = {...globalChartOptions,
+                      hAxis: {title: 'F_D',
+                              viewWindowMode: 'explicit',
+                              viewWindow: {min: 1.46, max: 1.48}},
+                      vAxis: {title: '\u03B8 (radians)',
+                              viewWindowMode: 'explicit',
+                              viewWindow: {min: 1, max: 3}},
+                      pointSize: 1, legend: 'none', //lineWidth: 0, 
+                      width: 500, height: 400},
+        detChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E20bddetChart")),
+//      detChart.draw(detTable, detOptions);
+        title = "<center><h3>PPM Bifurcation Diagram & Detail</h3></center>";
+        dsEBIiH("GiorCPC3E20Title", title);
+
+  let FD = FDmin;
   
   function makeC3E20Graph() {
     function dxdt(x, t) { // For use with RK42Dnonauton
@@ -22,66 +50,82 @@ function GiorCPC3E20() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 20
       let temp = -1*Math.sin(x1) - [x2]/2 + FD*Math.sin(WD*t);
       return [x2, temp];
     }
-    const mainData = [['FD', 'theta']];
-    const detData = [['FD', 'theta']];
-    let fn = [0.2, 0];
-    let fnp1 = [0, 0];
-    let lb = 0, ub = 0;
-    let FD = FDmin;
-    while (FD < FDmax) {
-      if (t >= tmin) {
-        if (!swtch) {
-          tdData.push([t, fn[0], fn[1]]);
-          ppData.push([fn[0], fn[1]]);
-        }
-        else {
-          lb = WDo2pi * (t-h2); ub = WDo2pi * (t+h2);
-          if (Math.ceil(lb) == Math.floor(ub)) {
-            saData.push([fn[0], fn[1]]);
-            if (!sadetswtch && fn[0] > 2) {
-              sadetswtch = true;
-            }
+    const swtch = swtchForm[0].checked;
+    if (swtch) {
+//      const mainData = [['FD', 'theta']];
+//      const detData = [['FD', 'theta']];
+      while (FD < FDmax) {
+        let t = 0;
+        let h = 0.01 * T;
+        let fn = [0.2, 0];
+        while (t < Xient) { // Calculate but "throw away" first 300 driving period results
+          fn = RK42Dnonauton(dxdt, fn, t, h); // 4th order RK for non-autonomous 2D system
+          if (fn[0] > pi) {
+            fn[0] -= tpi;
           }
+          else if (fn[0] < npi) {
+            fn[0] += tpi;
+          }
+          t += h;
         }
+        h /= 10;
+        let h2 = h/2; 
+        let nT = 301;
+        while (t <= tmax) { // Begin accumulating results
+          fn = RK42Dnonauton(dxdt, fn, t, h); // 4th order RK for non-autonomous 2D system
+          if (fn[0] > pi) {
+            fn[0] -= tpi;
+          }
+          else if (fn[0] < npi) {
+            fn[0] += tpi;
+          }
+          if (Math.abs(t - nT * T) < h2) {
+            mainData.push([FD, fn[0]]);
+            detData.push([FD, fn[0]]);
+//            mainChart.draw(google.visualization.arrayToDataTable(mainData), mainOptions);
+//            detChart.draw(google.visualization.arrayToDataTable(detData), detOptions);
+            nT += 1;
+          }
+          t += h;
+        }
+        FD += dFDmain;
+//        let check = (Math.floor(200*FD)==Math.ceil(200*FD));
+//        if (check) {
+//          mainChart.draw(google.visualization.arrayToDataTable(mainData), mainOptions);
+//          detChart.draw(google.visualization.arrayToDataTable(detData), detOptions);
+//        }
       }
-      fnp1 = RK42Dnonauton(dxdt, fn, t, h); // 4th order RK for non-autonomous 2D system
-      if (fnp1[0] > pi) {
-        fnp1[0] -= tpi;
-      }
-      else if (fnp1[0] < npi) {
-        fnp1[0] += tpi;
-      }
-      t += h;
-      fn = fnp1;
-    }
-      // Main chart creation and placement
-    const mainTable = google.visualization.arrayToDataTable(mainData);
-    const mainOptions = {...globalChartOptions,
-                         hAxis: {title: 'F<sub>D</sub>'},
-                         vAxis: {title: '\u03B8 (radians)',
-                                 viewWindowMode: 'explicit',
-                                 viewWindow: {min: 1, max: 3}},
-                         pointSize: 1, lineWidth: 0, legend: 'none', 
-                         width: 500, height: 400};
-    const mainChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E20bifdiaChart"));
-    mainChart.draw(mainTable, mainOptions);
+    // Main chart creation and placement
+/*      const mainTable = google.visualization.arrayToDataTable(mainData);
+      const mainOptions = {...globalChartOptions,
+                           hAxis: {title: 'F_D'},
+                           vAxis: {title: '\u03B8 (radians)',
+                                   viewWindowMode: 'explicit',
+                                   viewWindow: {min: 1, max: 3}},
+                           pointSize: 1, lineWidth: 0, legend: 'none', 
+                           width: 500, height: 400};
+      const mainChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E20bifdiaChart"));
+      mainChart.draw(mainTable, mainOptions);
 
-      // Detail chart creation and placement
-    const detTable = google.visualization.arrayToDataTable(detData);
-    const detOptions = {...globalChartOptions,
-                         hAxis: {title: 'F<sub>D</sub>'},
-                                 viewWindowMode: 'explicit',
-                                 viewWindow: {min: 1.46, max: 1.48}},
-                         vAxis: {title: '\u03B8 (radians)',
-                                 viewWindowMode: 'explicit',
-                                 viewWindow: {min: 1, max: 3}},
-                         pointSize: 1, lineWidth: 0, legend: 'none', 
-                         width: 500, height: 400};
+    // Detail chart creation and placement
+      const detTable = google.visualization.arrayToDataTable(detData);
+      const detOptions = {...globalChartOptions,
+                          hAxis: {title: 'F_D',
+                                  viewWindowMode: 'explicit',
+                                  viewWindow: {min: 1.46, max: 1.48}},
+                          vAxis: {title: '\u03B8 (radians)',
+                                  viewWindowMode: 'explicit',
+                                  viewWindow: {min: 1, max: 3}},
+                          pointSize: 1, lineWidth: 0, legend: 'none', 
+                          width: 500, height: 400};
       const detChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E20bddetChart"));
       detChart.draw(detTable, detOptions);
-    const title = "<center><h3>Solutions of Physical Pendulum Model</h3></center>";
-    dsEBIiH("GiorCPC3E20Title", title);
+      const title = "<center><h3>PPM Bifurcation Diagram & Detail</h3></center>";
+      dsEBIiH("GiorCPC3E20Title", title);
+*/
+    }
+    mainChart.draw(google.visualization.arrayToDataTable(mainData), mainOptions);
+    detChart.draw(google.visualization.arrayToDataTable(detData), detOptions);
   }
-  makeC3E20Graph();
   return makeC3E20Graph;
 }
