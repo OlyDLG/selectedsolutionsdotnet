9edf046997991224e6ee0f58c639585737363374
David L. Goldsmith <OlyDLG@users.noreply.github.com>
2025-09-11 11:19:40 -0700

    Given up on trying to get the BD plots updated...
    ...while they're being created.

--- Giordano/CompPhys/ExerciseSolutions/C3/C3E20.js
+++ Giordano/CompPhys/ExerciseSolutions/C3/C3E20.js
@@ -14,7 +14,6 @@ function GiorCPC3E20() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 20
         dFDdetail = 0.0001,
         swtchForm = document.getElementById("C3E20swtch"),
         mainData = [['FD', 'theta']],
-//        mainTable = google.visualization.arrayToDataTable(mainData),
         mainOptions = {...globalChartOptions,
                        hAxis: {title: 'F_D',
                                viewWindowMode: 'explicit',
@@ -25,9 +24,7 @@ function GiorCPC3E20() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 20
                        pointSize: 1, legend: 'none', //lineWidth: 0,  
                        width: 500, height: 400},
         mainChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E20bifdiaChart")),
-//      mainChart.draw(mainTable, mainOptions);
         detData = [['FD', 'theta']],
-//        detTable = google.visualization.arrayToDataTable(detData),
         detOptions = {...globalChartOptions,
                       hAxis: {title: 'F_D',
                               viewWindowMode: 'explicit',
@@ -38,22 +35,25 @@ function GiorCPC3E20() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 20
                       pointSize: 1, legend: 'none', //lineWidth: 0, 
                       width: 500, height: 400},
         detChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E20bddetChart")),
-//      detChart.draw(detTable, detOptions);
         title = "<center><h3>PPM Bifurcation Diagram & Detail</h3></center>";
-        dsEBIiH("GiorCPC3E20Title", title);
+  dsEBIiH("GiorCPC3E20Title", title);
 
   let FD = FDmin;
   
   function makeC3E20Graph() {
+
     function dxdt(x, t) { // For use with RK42Dnonauton
       let [x1, x2] = x;
       let temp = -1*Math.sin(x1) - [x2]/2 + FD*Math.sin(WD*t);
       return [x2, temp];
     }
+
+    function draw(chart, data, options) {
+      chart.draw(google.visualization.arrayToDataTable(data), options);
+    }
+
     const swtch = swtchForm[0].checked;
     if (swtch) {
-//      const mainData = [['FD', 'theta']];
-//      const detData = [['FD', 'theta']];
       while (FD < FDmax) {
         let t = 0;
         let h = 0.01 * T;
@@ -82,50 +82,16 @@ function GiorCPC3E20() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 20
           if (Math.abs(t - nT * T) < h2) {
             mainData.push([FD, fn[0]]);
             detData.push([FD, fn[0]]);
-//            mainChart.draw(google.visualization.arrayToDataTable(mainData), mainOptions);
-//            detChart.draw(google.visualization.arrayToDataTable(detData), detOptions);
             nT += 1;
           }
           t += h;
         }
         FD += dFDmain;
-//        let check = (Math.floor(200*FD)==Math.ceil(200*FD));
-//        if (check) {
-//          mainChart.draw(google.visualization.arrayToDataTable(mainData), mainOptions);
-//          detChart.draw(google.visualization.arrayToDataTable(detData), detOptions);
-//        }
       }
     // Main chart creation and placement
-/*      const mainTable = google.visualization.arrayToDataTable(mainData);
-      const mainOptions = {...globalChartOptions,
-                           hAxis: {title: 'F_D'},
-                           vAxis: {title: '\u03B8 (radians)',
-                                   viewWindowMode: 'explicit',
-                                   viewWindow: {min: 1, max: 3}},
-                           pointSize: 1, lineWidth: 0, legend: 'none', 
-                           width: 500, height: 400};
-      const mainChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E20bifdiaChart"));
-      mainChart.draw(mainTable, mainOptions);
-
-    // Detail chart creation and placement
-      const detTable = google.visualization.arrayToDataTable(detData);
-      const detOptions = {...globalChartOptions,
-                          hAxis: {title: 'F_D',
-                                  viewWindowMode: 'explicit',
-                                  viewWindow: {min: 1.46, max: 1.48}},
-                          vAxis: {title: '\u03B8 (radians)',
-                                  viewWindowMode: 'explicit',
-                                  viewWindow: {min: 1, max: 3}},
-                          pointSize: 1, lineWidth: 0, legend: 'none', 
-                          width: 500, height: 400};
-      const detChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E20bddetChart"));
-      detChart.draw(detTable, detOptions);
-      const title = "<center><h3>PPM Bifurcation Diagram & Detail</h3></center>";
-      dsEBIiH("GiorCPC3E20Title", title);
-*/
     }
-    mainChart.draw(google.visualization.arrayToDataTable(mainData), mainOptions);
-    detChart.draw(google.visualization.arrayToDataTable(detData), detOptions);
+    draw(mainChart, mainData, mainOptions);
+    draw(detChart, detData, detOptions);
   }
   return makeC3E20Graph;
 }
