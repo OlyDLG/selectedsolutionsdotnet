f743e935b8eb1ec7cc88be1ae33e5717ad0dc4c5
David L. Goldsmith <OlyDLG@users.noreply.github.com>
2025-10-12 15:19:54 -0700

    C3E26n7 Basically working,
    Need to polish it up.

--- Giordano/CompPhys/ExerciseSolutions/C3/C3E26n7.js
+++ Giordano/CompPhys/ExerciseSolutions/C3/C3E26n7.js
@@ -1,128 +1,99 @@
 function GiorCPC3E26n7() { // Giordano/Nakanishi Comp. Phys. Chpt. 3 Ex. 26/27
   const s = 10.0;
   const b = 8.0/3;
-/*
   const rForm = document.getElementById("C3E26n7r");
   const x0Form = document.getElementById("C3E26n7x0");
   const y0Form = document.getElementById("C3E26n7y0");
   const z0Form = document.getElementById("C3E26n7z0");
-*/
 
   function makeC3E26n7Graph() {
     function dxdt(x, t) { // For use with RK4nonauton
       let [x1, x2, x3] = x;
       let x1dot = s*(x2 - x1);
       let x2dot = (r - x3)*x1 - x2;
-      let x3dot = x1*x2 - b*x3);
+      let x3dot = x1*x2 - b*x3;
       return [x1dot, x2dot, x3dot];
     }
-    const r = 5,//Number(rForm[0].value),
-          x0 = 1,//Number(x0Form[0].value),
-          y0 = 0,//Number(y0Form[0].value),
-          z0 = 0,//Number(z0Form[0].value),
-    const h = 0.01,
-//    const h2 = h / 2;
-    const tXient = 30,
-//    const N = (!swtch) ? 1500 : 150000;
-    const tmax = 60;
-    let t = 0;
-    let fn = [0.2, 0];
-    let fnp1 = [0, 0];
-    const tdData = [['t', '\u03B8(t)', '\u03C9(t)']];
-    const ppData = [['\u03B8', '\u03C9']];
-    const saData = [['\u03B8', '\u03C9']];
-    let sadetswtch = false;
-    let lb = 0, ub = 0;
+    const r = Number(rForm[0].value),
+          x0 = Number(x0Form[0].value),
+          y0 = Number(y0Form[0].value),
+          z0 = Number(z0Form[0].value),
+          h = 0.01,
+          tXient = 30,
+          tmax = 60,
+          xvtData = [['t', 'x']],
+          yvtData = [['t', 'y']],
+          zvtData = [['t', 'z']],
+          yvxData = [['x', 'y']],
+          zvxData = [['x', 'z']],
+          zvyData = [['y', 'z']],
+          yvxPData = [['x', 'y']],
+          zvxPData = [['x', 'z']],
+          zvyPData = [['y', 'z']];
+
+    let t = 0,
+        fn = [x0, y0, z0];
+        fnp1 = [0, 0, 0];
     while (t < tmax) {
-      if (t >= tmin) {
-        if (!swtch) {
-          tdData.push([t, fn[0], fn[1]]);
-          ppData.push([fn[0], fn[1]]);
+      xvtData.push([t,fn[0]]);
+      yvtData.push([t,fn[1]]);
+      zvtData.push([t,fn[2]]);
+      yvxData.push([fn[0],fn[1]]);
+      zvxData.push([fn[0],fn[2]]);
+      zvyData.push([fn[1],fn[2]]);
+      fnp1 = RK4nonauton(dxdt, fn, t, h); // 4th order RK
+      if (t => tXient) {
+        if (fn[0]*fnp1[0] < 0) { // x(t) crossed zero
+          zvyPData.push([0.5*(fn[1]+fnp1[1]), 0.5*(fn[2]+fnp1[2])]);
         }
-        else {
-          lb = WDo2pi * (t-h2); ub = WDo2pi * (t+h2);
-          if (Math.ceil(lb) == Math.floor(ub)) {
-            saData.push([fn[0], fn[1]]);
-            if (!sadetswtch && fn[0] > 2) {
-              sadetswtch = true;
-            }
-          }
+        if (fn[1]*fnp1[1] < 0) { // y(t) crossed zero
+          zvxPData.push([0.5*(fn[0]+fnp1[0]), 0.5*(fn[2]+fnp1[2])]);
         }
-      }
-      fnp1 = RK4nonauton(dxdt, fn, t, h); // 4th order RK for non-autonomous 2D system
-      if (fnp1[0] > pi) {
-        fnp1[0] -= tpi;
-      }
-      else if (fnp1[0] < npi) {
-        fnp1[0] += tpi;
+        if (fn[2]*fnp1[2] < 0) { // z(t) crossed zero
+          yvxPData.push([0.5*(fn[0]+fnp1[0]), 0.5*(fn[1]+fnp1[1])]);
+        }
       }
       t += h;
       fn = fnp1;
     }
-    if (!swtch) { // Time domain chart creation and placement
-      const tdTable = google.visualization.arrayToDataTable(tdData);
-      const tdOptions = {...globalChartOptions,
-                         hAxis: {title: 'Time'},
-                         vAxis: {title: '\u03B8: radians, \u03C9: rad/sec',
-                                 viewWindowMode: 'explicit',
-                                 viewWindow: {min: -3.5, max: 3.5}},
-                         width: 500, height: 400};
-      const tdChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E16tdChart"));
-      tdChart.draw(tdTable, tdOptions);
+    const xvtDTable = google.visualization.arrayToDataTable(xvtData),
+          yvtDTable = google.visualization.arrayToDataTable(yvtData),
+          zvtDTable = google.visualization.arrayToDataTable(zvtData),
+          yvxDTable = google.visualization.arrayToDataTable(yvxData),
+          zvxDTable = google.visualization.arrayToDataTable(zvxData),
+          zvyDTable = google.visualization.arrayToDataTable(zvyData),
+          yvxPDTable = google.visualization.arrayToDataTable(yvxPData),
+          zvxPDTable = google.visualization.arrayToDataTable(zvxPData),
+          zvyPDTable = google.visualization.arrayToDataTable(zvyPData);
+    const Options = {...globalChartOptions,
+//                         hAxis: {title: 'Time'},
+//                         vAxis: {title: '\u03B8: radians, \u03C9: rad/sec',
+//                                 viewWindowMode: 'explicit',
+//                                 viewWindow: {min: -3.5, max: 3.5}},
+                         width: 300, height: 300};
+    const xvtChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7xvtChart"));
+    xvtChart.draw(xvtDTable, Options);
+    const yvtChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7yvtChart"));
+    yvtChart.draw(yvtDTable, Options);
+    const zvtChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7zvtChart"));
+    zvtChart.draw(zvtDTable, Options);
 
-        // Phase plane chart creation and placement
-      const ppTable = google.visualization.arrayToDataTable(ppData);
-      const ppOptions = {...globalChartOptions,
-                         hAxis: {title: '\u03B8 (radians)',
-                                 ticks: [{v: npi, f: '-\u03C0'},
-                                         {v: 0, f: '0'},
-                                         {v: pi, f: '\u03C0'}]},
-                         vAxis: {title: '\u03C9 (rad/sec)',
-                                 viewWindowMode: 'explicit',
-                                 viewWindow: {min: -2.5, max: 2.5}},
-                         pointSize: 1, lineWidth: 0, legend: 'none', 
-                         width: 500, height: 400};
-      const ppChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16ppChart"));
-      ppChart.draw(ppTable, ppOptions);
-    }
-    else { // Strange attractor chart creation and placement
-      const saTable = google.visualization.arrayToDataTable(saData);
-      const saOptions = {...globalChartOptions,
-                         hAxis: {title: '\u03B8 (radians)',
-                                 ticks: [{v: npi, f: '-\u03C0'},
-                                         {v: 0, f: '0'},
-                                         {v: pi, f: '\u03C0'}]},
-                         vAxis: {title: '\u03C9 (rad/sec)',
-                                 viewWindowMode: 'explicit',
-                                 viewWindow: {min: -2, max: 1}},
-                         pointSize: 1, lineWidth: 0, legend: 'none', 
-                         width: 500, height: 400};
-      const saChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16saChart"));
-      saChart.draw(saTable, saOptions);
+    const yvxChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7yvxChart"));
+    yvxChart.draw(yvxDTable, Options);
+    const zvxChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7zvxChart"));
+    zvxChart.draw(zvxDTable, Options);
+    const zvyChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7zvyChart"));
+    zvyChart.draw(zvyDTable, Options);
 
-        // Strange attractor detail chart creation and placement
-      if (sadetswtch) {
-        const detOptions = {...globalChartOptions,
-                            hAxis: {title: '\u03B8 (radians)',
-                                    ticks: [{v: 2, f: '2'},
-                                            {v: pi, f: '\u03C0'}],
-                                    viewWindowMode: 'explicit',
-                                    viewWindow: {min: 2.0, max: pi}
-                                    },
-                            vAxis: {title: '\u03C9 (rad/sec)'//,
-				 },
-                            pointSize: 1, lineWidth: 0, legend: 'none', 
-                            width: 500, height: 400};
-        const detChart = new google.visualization.ScatterChart(document.getElementById("GiorCPC3E16sadetChart"));
-        detChart.draw(saTable, detOptions);
-      }
-      else {
-        dsEBIiH("GiorCPC3E16sadetChart", "No Points in phase with \u03A9 = " + WD.toFixed(3) + "\nwith \u03B8 > 2");
-      }
-    }
-    const title = "<center><h3>Solutions of Physical Pendulum Model</h3></center>";
-    dsEBIiH("GiorCPC3E16Title", title);
+    const yvxPChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7yvxPChart"));
+    yvxPChart.draw(yvxPDTable, Options);
+
+    const zvxPChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7zvxPChart"));
+    zvxPChart.draw(zvxPDTable, Options);
+
+    const zvyPChart = new google.visualization.LineChart(document.getElementById("GiorCPC3E26n7zvyPChart"));
+    zvyPChart.draw(zvyPDTable, Options);
   }
-  makeC3E16Graph();
-  return makeC3E16Graph;
+  makeC3E26n7Graph();
+  return makeC3E26n7Graph;
 }
